0c43b4a14e8be2fcbf69ad9dfcc1c0127ca9b1ca
Kyle David <kyle.david@redline-electronics.com>
2013-04-25 17:34:09 -0400

    lists paired devices, prompts to search for devices

--- src/edu/kettering/blackbox/BluetoothReceiver.java
+++ src/edu/kettering/blackbox/BluetoothReceiver.java
@@ -1,8 +1,10 @@
 package edu.kettering.blackbox;
 
+import android.bluetooth.BluetoothAdapter;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class BluetoothReceiver extends BroadcastReceiver {
 	public BluetoothReceiver() {
@@ -12,6 +14,27 @@ public class BluetoothReceiver extends BroadcastReceiver {
 	public void onReceive(Context context, Intent intent) {
 		// TODO: This method is called when the BroadcastReceiver is receiving
 		// an Intent broadcast.
-		throw new UnsupportedOperationException("Not yet implemented");
+		switch (intent.getIntExtra(BluetoothAdapter.EXTRA_STATE, BluetoothAdapter.STATE_OFF)) {
+//		case BluetoothAdapter.STATE_CONNECTED:
+//			break;
+//		case BluetoothAdapter.STATE_CONNECTING:
+//			break;
+//		case BluetoothAdapter.STATE_DISCONNECTED:
+//			break;
+//		case BluetoothAdapter.STATE_DISCONNECTING:
+//			break;
+		case BluetoothAdapter.STATE_OFF:
+			//TODO show dialog warning user that bluetooth must be on to use this app
+			Toast.makeText(context, "Bluetooth must be enabled to use this app", Toast.LENGTH_SHORT).show();
+			break;
+		case BluetoothAdapter.STATE_ON:
+			break;
+		case BluetoothAdapter.STATE_TURNING_OFF:
+			break;
+		case BluetoothAdapter.STATE_TURNING_ON:
+			break;
+		default:
+				
+		}
 	}
 }
--- src/edu/kettering/blackbox/MainActivity.java
+++ src/edu/kettering/blackbox/MainActivity.java
@@ -1,22 +1,33 @@
 package edu.kettering.blackbox;
 
-import android.os.Bundle;
+import java.util.Set;
+
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	private static final int REQUEST_ENABLE_BT = 1;
+	private BluetoothReceiver btReceiver;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         TextView textView = (TextView)findViewById(R.id.textView1);
+        ListView listViewPairedDevices = (ListView)findViewById(R.id.listViewPairedDevices);
+        // create an array adapter for the listview
+        ArrayAdapter<String> mArrayAdapterPairedDevices = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        
         // set up bluetooth
         BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
         if (mBluetoothAdapter == null) {
@@ -35,7 +46,51 @@ public class MainActivity extends Activity {
         }
         
         // add event listener for changes in bluetooth state (such as being turned off during operation)
-        IntentFilter listenBtIntent = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
+        registerReceiver(btReceiver = new BluetoothReceiver(), new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED));
+        
+        //TODO paired device list not populated if bluetooth is not enabled before starting the app
+        // find devices (first look through paired devices to find the correct one)
+        Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+        if (pairedDevices.size() > 0) {        	
+        	// Loop through paired devices
+        	for (BluetoothDevice device : pairedDevices) {
+        		// Add the name and address to an array adapter to show in a ListView
+        		mArrayAdapterPairedDevices.add(device.getName() + "\n" + device.getAddress());
+        	}
+        	listViewPairedDevices.setAdapter(mArrayAdapterPairedDevices);
+        }
+        
+/*        // discover new devices
+        if (!mBluetoothAdapter.startDiscovery()) {
+        	Toast.makeText(this, "Discovery failed", Toast.LENGTH_SHORT).show();
+        	return;
+        }
+      
+        // receiver for when a device is found
+        final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+			@Override
+			public void onReceive(Context context, Intent intent) {
+				String action = intent.getAction();
+				// When discovery finds a device
+				if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+					// get the BluetoothDevice object from the Intent
+					BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+					// add the name and address to an array adapter to show in a listview
+					mArrayAdapterNewDevices.add(device.getName() + "\n" + device.getAddress());
+				}				
+			}        	
+        };
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+*/        
+        // If the correct device is not already paired, show the bluetooth settings window to let the user pair the device
+//        startActivity(new Intent(android.provider.Settings.ACTION_BLUETOOTH_SETTINGS));
+        
+        
+        // connect to the device
+        
+        
     }
 
 
@@ -53,10 +108,19 @@ public class MainActivity extends Activity {
     		if (resultCode == RESULT_OK) {
     			//TODO catch bt error, or user did not enable bt
     			Toast.makeText(getApplicationContext(), "Bluetooth enabled", Toast.LENGTH_SHORT).show();
+    		} else if (resultCode == RESULT_CANCELED){
+    			Toast.makeText(getApplicationContext(), "Error: Bluetooth must be enabled for this app to work", Toast.LENGTH_LONG).show();
     		}
     		break;
     	default:
     		
     	}
+    }
+    
+    @Override
+    protected void onDestroy() {
+    	// unregister BroadcastReceiver
+    	unregisterReceiver(btReceiver);
+    	super.onDestroy();
     }
 }
